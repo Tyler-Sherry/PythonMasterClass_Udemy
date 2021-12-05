c59c76347b696aa59a9439becda36f5848175a9c
Armored Carnage <67198813+ArmoredCarnage@users.noreply.github.com>
2021-12-05 14:27:31 -0600

    Initial Commit for Guessing Game Udemy Course

---
+++ GuessingGame.py
@@ -0,0 +1,36 @@
+answer = 5
+
+print("Please guess a number between 1 and 10:  ")
+guess = int(input())
+
+if guess == answer:
+    print("You got it the first time.")
+
+else:
+    if guess < answer:
+        print("Please guess higher")
+    else:   # guess must be greater than answer
+        print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry you have not guessed correctly")
+
+# if guess < answer:
+#     print("Please guess higher")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed the it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# elif guess > answer:
+#     print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed the it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# else:
+#     print("You guessed correctly the first time")
+
